326ec3b4ebcb6686723478e3438f0102c95a9e5e
Monique Bicudo <monique.bicudo@escola.pr.gov.br>
2025-11-28 23:46:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+function openMenu() {
+    const menu = document.getElementById("menu");
+    const btn = document.querySelector(".menu-btn");
+
+    menu.style.width = "250px";
+    btn.classList.add("active");
+}
+
+function closeMenu() {
+    const menu = document.getElementById("menu");
+    const btn = document.querySelector(".menu-btn");
+
+    menu.style.width = "0";
+    btn.classList.remove("active");
+}
+
+document.addEventListener("click", function (event) {
+    const menu = document.getElementById("menu");
+    const menuBtn = document.querySelector(".menu-btn");
+
+    if (!menu.contains(event.target) &&
+        !menuBtn.contains(event.target)) {
+        closeMenu();
+    }
+});
 
